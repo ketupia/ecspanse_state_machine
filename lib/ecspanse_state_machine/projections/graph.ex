1f679c5d4cafb1a2d8b329a18072ab25c5e981fc
ketupia <ketupia@gmail.com>
2024-03-02 12:41:22 -0500

    Projections * removed the ability to project a node by name

--- lib/ecspanse_state_machine/projections/graph.ex
+++ lib/ecspanse_state_machine/projections/graph.ex
@@ -12,26 +12,35 @@ defmodule EcspanseStateMachine.Projections.Graph do
       :starting_node_name,
       :is_running,
       :current_node_name,
-      :current_node
+      :current_node,
+      :nodes
     ]
 
   @impl true
   def project(%{entity_id: entity_id} = _attrs) do
-    IO.inspect(entity_id, label: "projecting graph")
-
     with {:ok, graph_entity} <- Ecspanse.Entity.fetch(entity_id),
          {:ok, graph_component} <- Components.Graph.fetch(graph_entity) do
       current_node_map = map_current_node(graph_component)
 
       fields =
         Map.to_list(graph_component) ++
-          [{:entity_id, entity_id}, {:current_node, current_node_map}]
+          [
+            {:entity_id, entity_id},
+            {:current_node, current_node_map}
+          ]
+
+      # , {:nodes, map_nodes(graph_component)}
 
       {:ok, struct!(__MODULE__, fields)}
     else
       _ -> :error
     end
   end
+
+  # defp map_nodes(graph_component) do
+  #   Locator.fetch_nodes(graph_component.name)
+  #   |> Enum.map(&project_node/1)
+  # end
 
   defp map_current_node(graph_component) do
     if graph_component.current_node_name == nil do
@@ -40,23 +49,25 @@ defmodule EcspanseStateMachine.Projections.Graph do
       {:ok, node_component} =
         Locator.fetch_node_component(graph_component, graph_component.current_node_name)
 
-      map =
-        Map.take(node_component, [
-          :name,
-          :allowed_exit_node_names,
-          :has_timer,
-          :timeout_node_name
-        ])
+      project_node(node_component)
+    end
+  end
 
+  defp project_node(node_component) do
+    map =
+      Map.take(node_component, [
+        :name,
+        :allowed_exit_node_names,
+        :has_timer,
+        :timeout_node_name
+      ])
+
+    if node_component.has_timer do
       node_entity = Ecspanse.Query.get_component_entity(node_component)
-      map = Map.put(map, :entity_id, node_entity.id)
-
-      if node_component.has_timer do
-        {:ok, timer_component} = Components.NodeTimeoutTimer.fetch(node_entity)
-        Map.put(map, :timeout_timer, Map.take(timer_component, [:duration, :time, :paused]))
-      else
-        map
-      end
+      {:ok, timer_component} = Components.NodeTimeoutTimer.fetch(node_entity)
+      Map.put(map, :timer, Map.take(timer_component, [:duration, :paused]))
+    else
+      map
     end
   end
 
