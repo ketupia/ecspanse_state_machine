4a349d0a537d60553d56729ccb05d91ee2a284bb
ketupia <ketupia@gmail.com>
2024-03-03 23:02:38 -0500

    Remove hello world test

--- test/ecspanse_state_machine_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule EcspanseStateMachineTest do
-  use ExUnit.Case, async: false
-  doctest EcspanseStateMachine
-
-  test "greets the world" do
-    assert EcspanseStateMachine.hello() == :world
-  end
-end
